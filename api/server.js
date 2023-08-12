ca3f3f675ca78a310c4a111e39979ba4122e8b20
Abdel <abdelbrahim945@gmail.com>
2023-08-12 01:11:57 -0700

    mongodb atlas setup

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const connectDB = require('./config/db');
 const app = express();
 
 app.use(express.json());
+
+//Connect mongo database
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 const port = process.env.PORT || 8082;
